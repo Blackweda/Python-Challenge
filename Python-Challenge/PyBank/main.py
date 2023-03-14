f7dd8eee35f09c7810355d51773ca3944a99b099
Wayne Williams <siloaman@hotmail.ca>
2023-03-14 11:40:45 -0400

    Add files via upload

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -8,7 +8,10 @@ months_count = 0
 
 profit_loss_total_amount = 0
 
-profit_loss_changes = []
+# Tracking month-to-month changes
+profit_loss_figures = []
+month_difference = 0
+profit_loss_figures_differences = []
 profit_loss_changes_average = 0
     
 # Tracking the greatest profit
@@ -33,36 +36,38 @@ with open(filepath, encoding='utf') as csvfile:
         # Every row add to the profit/loss amount
         profit_loss_total_amount = profit_loss_total_amount + int(row[1])
 
+        # Keep record of this month's figures
+        profit_loss_figures.append(int(row[1]))
+                
         # Track the changes in profit/loss and average the results
-        if months_count == 1:
-            profit_loss_changes.append(int(row[1]))
-            print("first entry: " + row[1] + " and size of plc: " + str(len(profit_loss_changes)))
-        else:    
-            profit_loss_changes.append(int(row[1]) - profit_loss_changes[len(profit_loss_changes)-1])
-            print("row1: " + row[1] + " minus: " + str((profit_loss_changes[len(profit_loss_changes)-1])) + " equals: " + str(int(row[1]) - profit_loss_changes[len(profit_loss_changes)-1]))
-      
-        # Determine if this row is the greatest profit (so far)
-        if int(row[1]) > greatest_profit_amount:
-            greatest_profit_amount = int(row[1])
+        if months_count > 1:    # if this is 2nd month moving forward (so profit_loss_figures size is greater than 1)
+            month_difference = profit_loss_figures[len(profit_loss_figures)-1] - profit_loss_figures[len(profit_loss_figures)-2]
+
+        # on the first row, both month_difference and greatest_profit(loss)_amount will be zero... therefore everything will adjust consequently
+        if month_difference > greatest_profit_amount:
+            greatest_profit_amount = month_difference
             greatest_profit_date = row[0]
 
-        # Determine if this row is the greatest loss (so far)
-        if int(row[1]) < greatest_loss_amount:
-            greatest_loss_amount = int(row[1])
+        if month_difference < greatest_loss_amount:
+            greatest_loss_amount = month_difference
             greatest_loss_date = row[0]
 
+        if months_count > 1:        # I don't want to have the initialization value of 0 skewing the averages
+            profit_loss_figures_differences.append(month_difference)    # keep track of the differences between months      
+         
+
     # Grab the average of the total profits/losses
-    for i in profit_loss_changes:
+    for i in profit_loss_figures_differences:
 
         profit_loss_changes_average += i
 
-    profit_loss_changes_average / len(profit_loss_changes)
+    profit_loss_changes_average = profit_loss_changes_average / len(profit_loss_figures_differences)
     
     print("Financial Analysis\n")
     print("------------------\n")    
     print(f"Total Months: {months_count}\n")
     print(f"Total: ${profit_loss_total_amount}\n")   
-    print(f"Average Change: ${profit_loss_changes_average}\n") 
+    print(f"Average Change: ${round(profit_loss_changes_average, 2)}\n") 
     print(f"Greatest Increase in Profits: {greatest_profit_date} (${greatest_profit_amount})\n")
     print(f"Greatest Decrease in Profits: {greatest_loss_date} (${greatest_loss_amount})")
 
@@ -73,7 +78,7 @@ with open(filepath, encoding='utf') as csvfile:
 
         mc = repr(months_count)
         plta = repr(profit_loss_total_amount)
-        plca = repr(profit_loss_changes_average)
+        plca = repr(round(profit_loss_changes_average, 2))
         gpa = repr(greatest_profit_amount)
         gla = repr(greatest_loss_amount)
         
